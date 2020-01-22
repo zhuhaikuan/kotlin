43ba21f8ea96071ea0885aed341801dab7a16c5d
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-01-22 03:50:05 +0300

    Improved kotlin decompiler range indexes
    Fixed #KT-30628

--- compiler/util/src/org/jetbrains/kotlin/type/MapPsiToAsmDesc.kt
+++ compiler/util/src/org/jetbrains/kotlin/type/MapPsiToAsmDesc.kt
@@ -59,8 +59,7 @@ object MapPsiToAsmDesc {
         append(")")
         psiMethod.returnType?.let {
             append(typeDesc(it))
-        }
-                ?: return unknownSignature() // TODO: support constructors, there seems to be additional logic in java that doesn't work correctly for compiled kotlin
+        } ?: append("V")
     }
 
     private fun unknownSignature() = ""
