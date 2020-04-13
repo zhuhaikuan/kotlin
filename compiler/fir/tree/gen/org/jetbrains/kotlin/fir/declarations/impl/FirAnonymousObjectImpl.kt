15c27cdcecd43610afbb9650aea824a7f73c1473
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-04-13 11:55:08 +0300

    [FIR] Add transform declaration and companion to regular class

--- compiler/fir/tree/gen/org/jetbrains/kotlin/fir/declarations/impl/FirAnonymousObjectImpl.kt
+++ compiler/fir/tree/gen/org/jetbrains/kotlin/fir/declarations/impl/FirAnonymousObjectImpl.kt
@@ -52,10 +52,15 @@ internal class FirAnonymousObjectImpl(
 
     override fun <D> transformChildren(transformer: FirTransformer<D>, data: D): FirAnonymousObjectImpl {
         superTypeRefs.transformInplace(transformer, data)
-        declarations.transformInplace(transformer, data)
+        transformDeclarations(transformer, data)
         transformAnnotations(transformer, data)
         typeRef = typeRef.transformSingle(transformer, data)
         transformControlFlowGraphReference(transformer, data)
+        return this
+    }
+
+    override fun <D> transformDeclarations(transformer: FirTransformer<D>, data: D): FirAnonymousObjectImpl {
+        declarations.transformInplace(transformer, data)
         return this
     }
 
