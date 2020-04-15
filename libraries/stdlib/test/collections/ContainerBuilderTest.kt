05f65171094b46e0c91dd7bb4886b7b47c8c62aa
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2020-04-15 06:08:27 +0300

    Add behavioral tests

--- libraries/stdlib/test/collections/ContainerBuilderTest.kt
+++ libraries/stdlib/test/collections/ContainerBuilderTest.kt
@@ -1,8 +1,40 @@
 package test.collections
 
+import test.collections.behaviors.listBehavior
+import test.collections.behaviors.mapBehavior
+import test.collections.behaviors.setBehavior
 import kotlin.test.*
 
 class ContainerBuilderTest {
+    private fun <E> mutableCollectionOperations(e: E) = listOf<MutableCollection<E>.() -> Unit>(
+        { add(e) },
+        { addAll(listOf(e)) },
+        { remove(e) },
+        { removeAll(listOf(e)) },
+        { retainAll(listOf(e)) },
+        { clear() },
+        { iterator().apply { next() }.remove() }
+    )
+
+    private fun <E> mutableListOperations(e: E) = mutableCollectionOperations(e) + listOf<MutableList<E>.() -> Unit>(
+        { add(0, e) },
+        { addAll(0, listOf(e)) },
+        { removeAt(0) },
+        { set(0, e) },
+        { listIterator().apply { next() }.remove() },
+        { listIterator(0).apply { next() }.remove() },
+        { listIterator().add(e) }
+    )
+
+    private fun <E> mutableSetOperations(e: E): List<MutableSet<E>.() -> Unit> = mutableCollectionOperations(e)
+
+    private fun <K, V> mutableMapOperations(k: K, v: V) = listOf<MutableMap<K, V>.() -> Unit>(
+        { put(k, v) },
+        { remove(k) },
+        { putAll(mapOf(k to v)) },
+        { clear() }
+    )
+
     @Test
     fun buildList() {
         val x = buildList {
@@ -16,11 +48,17 @@ class ContainerBuilderTest {
             add('d')
         }
 
-        assertEquals(listOf('a', 'b', 'c', 'd'), y)
+        compare(listOf('a', 'b', 'c', 'd'), y) { listBehavior() }
 
         assertEquals(listOf(1), buildList(0) { add(1) })
         assertFailsWith<IllegalArgumentException> {
             buildList(-1) { add(0) }
+        }
+
+        assertTrue(y is MutableList<Char>)
+        for (operation in mutableListOperations('a')) {
+            assertFailsWith<UnsupportedOperationException> { y.operation() }
+            assertFailsWith<UnsupportedOperationException> { y.subList(1, 3).operation() }
         }
     }
 
@@ -37,11 +75,16 @@ class ContainerBuilderTest {
             add('d')
         }
 
-        assertEquals(setOf('c', 'b', 'd'), y)
+        compare(setOf('c', 'b', 'd'), y) { setBehavior() }
 
         assertEquals(setOf(1), buildSet(0) { add(1) })
         assertFailsWith<IllegalArgumentException> {
             buildSet(-1) { add(0) }
+        }
+
+        assertTrue(y is MutableSet<Char>)
+        for (operation in mutableSetOperations('b')) {
+            assertFailsWith<UnsupportedOperationException> { y.operation() }
         }
     }
 
@@ -59,11 +102,25 @@ class ContainerBuilderTest {
             put('d', 4)
         }
 
-        assertEquals(mapOf('a' to 1, 'c' to 3, 'b' to 2, 'd' to 4), y)
+        compare(mapOf('a' to 1, 'c' to 3, 'b' to 2, 'd' to 4), y) { mapBehavior() }
 
         assertEquals(mapOf("a" to 1), buildMap<String, Int>(0) { put("a", 1) })
         assertFailsWith<IllegalArgumentException> {
             buildMap<String, Int>(-1) { put("x", 1) }
+        }
+
+        assertTrue(y is MutableMap<Char, Int>)
+        for (operation in mutableMapOperations('a', 0)) {
+            assertFailsWith<UnsupportedOperationException> { y.operation() }
+        }
+        for (operation in mutableSetOperations('a')) {
+            assertFailsWith<UnsupportedOperationException> { y.keys.operation() }
+        }
+        for (operation in mutableCollectionOperations(1)) {
+            assertFailsWith<UnsupportedOperationException> { y.values.operation() }
+        }
+        for (operation in mutableSetOperations(y.entries.first())) {
+            assertFailsWith<UnsupportedOperationException> { y.entries.operation() }
         }
     }
 }
