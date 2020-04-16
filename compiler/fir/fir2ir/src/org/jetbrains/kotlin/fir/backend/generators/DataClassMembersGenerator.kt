4ee4a012694251a7d5108acde64c8fa0f6ab53a0
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-04-16 09:50:07 +0300

    Use FIR metadata in FIR2IR data class member generator

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/generators/DataClassMembersGenerator.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/generators/DataClassMembersGenerator.kt
@@ -10,7 +10,19 @@ import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.fir.backend.Fir2IrComponents
+import org.jetbrains.kotlin.fir.backend.FirMetadataSource
 import org.jetbrains.kotlin.fir.backend.declareThisReceiverParameter
+import org.jetbrains.kotlin.fir.declarations.FirClass
+import org.jetbrains.kotlin.fir.declarations.builder.buildSimpleFunction
+import org.jetbrains.kotlin.fir.declarations.builder.buildValueParameter
+import org.jetbrains.kotlin.fir.declarations.impl.FirDeclarationStatusImpl
+import org.jetbrains.kotlin.fir.symbols.CallableId
+import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirVariableSymbol
+import org.jetbrains.kotlin.fir.types.impl.FirImplicitBooleanTypeRef
+import org.jetbrains.kotlin.fir.types.impl.FirImplicitIntTypeRef
+import org.jetbrains.kotlin.fir.types.impl.FirImplicitNullableAnyTypeRef
+import org.jetbrains.kotlin.fir.types.impl.FirImplicitStringTypeRef
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
 import org.jetbrains.kotlin.ir.builders.IrGeneratorContextBase
 import org.jetbrains.kotlin.ir.declarations.*
@@ -21,6 +33,7 @@ import org.jetbrains.kotlin.ir.descriptors.WrappedValueParameterDescriptor
 import org.jetbrains.kotlin.ir.expressions.IrMemberAccessExpression
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.util.DataClassMembersGenerator
+import org.jetbrains.kotlin.ir.util.classId
 import org.jetbrains.kotlin.ir.util.defaultType
 import org.jetbrains.kotlin.name.Name
 
@@ -28,11 +41,12 @@ class DataClassMembersGenerator(val components: Fir2IrComponents) {
 
     // TODO: generateInlineClassMembers
 
-    fun generateDataClassMembers(irClass: IrClass): List<Name> =
-        MyDataClassMethodsGenerator(irClass, IrDeclarationOrigin.GENERATED_DATA_CLASS_MEMBER).generate()
+    fun generateDataClassMembers(irClass: IrClass, firClass: FirClass<*>): List<Name> =
+        MyDataClassMethodsGenerator(irClass, firClass, IrDeclarationOrigin.GENERATED_DATA_CLASS_MEMBER).generate()
 
     private inner class MyDataClassMethodsGenerator(
         val irClass: IrClass,
+        val firClass: FirClass<*>,
         val origin: IrDeclarationOrigin
     ) {
         val properties = irClass.declarations.filterIsInstance<IrProperty>().map { it.descriptor }
@@ -78,11 +92,7 @@ class DataClassMembersGenerator(val components: Fir2IrComponents) {
             val equalsFunction = createSyntheticIrFunction(
                 Name.identifier("equals"),
                 components.irBuiltIns.booleanType,
-            ).apply {
-                valueParameters = listOf(
-                    createSyntheticIrParameter(this, Name.identifier("other"), components.irBuiltIns.anyNType)
-                )
-            }
+            ) { createSyntheticIrParameter(it, Name.identifier("other"), components.irBuiltIns.anyNType) }
             irDataClassMembersGenerator.generateEqualsMethod(equalsFunction, properties)
 
             val hashCodeFunction = createSyntheticIrFunction(
@@ -100,7 +110,11 @@ class DataClassMembersGenerator(val components: Fir2IrComponents) {
             return listOf(equalsFunction.name, hashCodeFunction.name, toStringFunction.name)
         }
 
-        private fun createSyntheticIrFunction(name: Name, returnType: IrType): IrFunction {
+        private fun createSyntheticIrFunction(
+            name: Name,
+            returnType: IrType,
+            valueParameterBuilder: (IrFunction) -> IrValueParameter? = { null }
+        ): IrFunction {
             val functionDescriptor = WrappedSimpleFunctionDescriptor()
             val thisReceiverDescriptor = WrappedValueParameterDescriptor()
             return components.symbolTable.declareSimpleFunction(UNDEFINED_OFFSET, UNDEFINED_OFFSET, origin, functionDescriptor) { symbol ->
@@ -121,7 +135,40 @@ class DataClassMembersGenerator(val components: Fir2IrComponents) {
                     isFakeOverride = false,
                     isOperator = false
                 ).apply {
-                    metadata = MetadataSource.Function(functionDescriptor)
+                    val irValueParameter = valueParameterBuilder(this)?.let {
+                        this.valueParameters = listOf(it)
+                        it
+                    }
+                    metadata = FirMetadataSource.Function(
+                        buildSimpleFunction {
+                            this.name = name
+                            val classId = firClass.symbol.classId
+                            this.symbol = FirNamedFunctionSymbol(CallableId(classId.packageFqName, classId.relativeClassName, name))
+                            this.status = FirDeclarationStatusImpl(Visibilities.PUBLIC, Modality.FINAL)
+                            this.session = components.session
+                            this.returnTypeRef = when (returnType) {
+                                components.irBuiltIns.booleanType -> FirImplicitBooleanTypeRef(null)
+                                components.irBuiltIns.intType -> FirImplicitIntTypeRef(null)
+                                components.irBuiltIns.stringType -> FirImplicitStringTypeRef(null)
+                                else -> throw AssertionError("Should not be here")
+                            }
+                            if (irValueParameter != null) {
+                                this.valueParameters.add(
+                                    buildValueParameter {
+                                        this.name = irValueParameter.name
+                                        this.session = components.session
+                                        this.returnTypeRef = FirImplicitNullableAnyTypeRef(null)
+                                        this.symbol = FirVariableSymbol(irValueParameter.name)
+                                        isCrossinline = false
+                                        isNoinline = false
+                                        isVararg = false
+                                    }
+                                )
+                            }
+                        },
+                        descriptor
+                    )
+
                 }
             }.apply {
                 parent = irClass
