7f68448dcf9ed22a5b03afce00cde0ac9e1ad066
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-04-21 13:15:29 +0700

    [Linker] Add option to control scope of DescriptorByIdSignatureFinder
    Currently DescriptorByIdSignatureFinder performs lookup of descriptor
    not only in the given module but also in its dependencies.
    This behaviour is incorrect if we have some kind of
    relation (e.g. mapping) between moduleDescriptor and found descriptor.
    Thus, we add a handle to control search scope.

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/DescriptorByIdSignatureFinder.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/DescriptorByIdSignatureFinder.kt
@@ -6,10 +6,31 @@
 package org.jetbrains.kotlin.backend.common.serialization
 
 import org.jetbrains.kotlin.descriptors.*
+import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
 import org.jetbrains.kotlin.ir.util.IdSignature
 import org.jetbrains.kotlin.ir.util.KotlinMangler
 
-class DescriptorByIdSignatureFinder(private val moduleDescriptor: ModuleDescriptor, private val mangler: KotlinMangler.DescriptorMangler) {
+class DescriptorByIdSignatureFinder(
+    private val moduleDescriptor: ModuleDescriptor,
+    private val mangler: KotlinMangler.DescriptorMangler,
+    private val lookupMode: LookupMode
+) {
+
+    /**
+     * Sets search scope for [findDescriptorBySignature].
+     */
+    enum class LookupMode {
+        /**
+         * Perform search of descriptor in [moduleDescriptor] and its dependencies.
+         */
+        MODULE_WITH_DEPENDENCIES,
+
+        /**
+         * Perform search of descriptor only in [moduleDescriptor].
+         */
+        MODULE_ONLY
+    }
+
     fun findDescriptorBySignature(signature: IdSignature): DeclarationDescriptor? = when (signature) {
         is IdSignature.AccessorSignature -> findDescriptorForAccessorSignature(signature)
         is IdSignature.PublicSignature -> findDescriptorForPublicSignature(signature)
@@ -24,11 +45,29 @@ class DescriptorByIdSignatureFinder(private val moduleDescriptor: ModuleDescript
         }
     }
 
+    private fun performLookup(signature: IdSignature.PublicSignature): Collection<DeclarationDescriptor> {
+        val declarationName = signature.declarationFqn.pathSegments().first()
+        return when (lookupMode) {
+            LookupMode.MODULE_WITH_DEPENDENCIES -> {
+                moduleDescriptor
+                    .getPackage(signature.packageFqName())
+                    .memberScope
+                    .getContributedDescriptors { name -> name == declarationName }
+            }
+            LookupMode.MODULE_ONLY -> {
+                (moduleDescriptor as ModuleDescriptorImpl)
+                    .packageFragmentProviderForModuleContentWithoutDependencies
+                    .getPackageFragments(signature.packageFqName())
+                    .map(PackageFragmentDescriptor::getMemberScope)
+                    .flatMap { it.getContributedDescriptors { name -> name == declarationName } }
+            }
+        }
+    }
+
     private fun findDescriptorForPublicSignature(signature: IdSignature.PublicSignature): DeclarationDescriptor? {
-        val packageDescriptor = moduleDescriptor.getPackage(signature.packageFqName())
+        val toplevelDescriptors = performLookup(signature)
+            .ifEmpty { return null }
         val pathSegments = signature.declarationFqn.pathSegments()
-        val toplevelDescriptors = packageDescriptor.memberScope.getContributedDescriptors { name -> name == pathSegments.first() }
-            .filter { it.name == pathSegments.first() }
         val candidates = pathSegments.drop(1).fold(toplevelDescriptors) { acc, current ->
             acc.flatMap { container ->
                 val classDescriptor = container as? ClassDescriptor
